b2e0e29892c544a5566646835114c77659a58a65
zachvp <vegaperk@gmail.com>
2025-10-22 15:44:09 -0400

    use constant values for tags_info UI

--- src/djmgmt/ui/pages/tags_info.py
+++ src/djmgmt/ui/pages/tags_info.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 import logging
 
-from djmgmt.tags_info import log_duplicates, compare_tags
+from djmgmt import tags_info
 from djmgmt import common
 from djmgmt.ui.utils import utils
 
@@ -10,29 +10,26 @@ from djmgmt.ui.utils import utils
 MODULE = 'tags_info'
 
 function = st.selectbox('Function', [
-        'log_duplicates',
-        'write_identifiers',
-        'compare'
+        tags_info.Namespace.FUNCTION_LOG_DUPLICATES,
+        tags_info.Namespace.FUNCTION_WRITE_PATHS,
+        tags_info.Namespace.FUNCTION_COMPARE
     ])
 
 input_path = st.text_input('Input Path', value=os.path.expanduser('~/Music/DJ'))
 
 comparison = None
-if function == 'compare':
+if function == tags_info.Namespace.FUNCTION_COMPARE:
     comparison = st.text_input('Comparison Path')
 
-# TODO: implement
-# output = st.text_input('Output File (optional)')
-
 if st.button('Run'):
-    if function == 'log_duplicates':
+    if function == tags_info.Namespace.FUNCTION_LOG_DUPLICATES:
         # Configure logging before running function
         log_path = utils.create_log_path(MODULE)
         common.configure_log(level=logging.DEBUG, path=str(log_path))
-        duplicates = log_duplicates(input_path)
+        duplicates = tags_info.log_duplicates(input_path)
         st.write("### Results:")
         st.dataframe(sorted(duplicates))
-    elif function == 'compare' and comparison:
-        results = compare_tags(input_path, comparison)
+    elif function == tags_info.Namespace.FUNCTION_COMPARE and comparison:
+        results = tags_info.compare_tags(input_path, comparison)
         st.write(f"Found {len(results)} changes")
         st.dataframe(results)
