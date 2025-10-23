42aa8c7328823c82f3e4135e0e9e3e5fe21e3770
zachvp <vegaperk@gmail.com>
2025-10-23 11:35:16 -0400

    display config values on app page

--- src/djmgmt/ui/app.py
+++ src/djmgmt/ui/app.py
@@ -1,8 +1,17 @@
 """Main Streamlit application for djmgmt toolkit."""
 import streamlit as st
+from djmgmt.ui.utils import config
 
 # Main UI
 st.title("djmgmt Tools")
+
+# Display Config
+st.write('### Config')
+st.dataframe(config.load().to_dict())
+
+st.write('---')
+
+# Call to action
 st.write('''
          #### 👈  Choose a module from the left sidebar.
 ''')
